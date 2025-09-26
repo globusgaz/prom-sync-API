c0074d8875f28db99777d31f668af3457cfb6130
globusgaz <48650612+globusgaz@users.noreply.github.com>
2025-09-26 10:56:27 +0300

    Update test_update.py

--- src/test_update.py
+++ src/test_update.py
@@ -1,15 +1,23 @@
 import requests
 import os
 import json
+import sys
 
-API_TOKEN = os.getenv("PROM_API_TOKEN")  # токен з GitHub Secrets
+API_TOKEN = os.getenv("PROM_API_TOKEN")
 API_URL = "https://my.prom.ua/api/v1/products/edit"
+
+if len(sys.argv) < 3:
+    print("❌ Використання: python test_update.py <SKU> <ЦІНА>")
+    sys.exit(1)
+
+sku = sys.argv[1]
+new_price = float(sys.argv[2])
 
 payload = {
     "products": [
         {
-            "external_id": "f4_2736731",  # тестовий SKU
-            "price": 1000000,             # тестова ціна
+            "external_id": sku,
+            "price": new_price,
             "quantity_in_stock": 5,
             "presence": "available"
         }
