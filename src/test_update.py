1a0ca1e0642760427c2ecd7c42afab46fe5aae86
globusgaz <48650612+globusgaz@users.noreply.github.com>
2025-09-28 22:13:26 +0300

    Update test_update.py

--- src/test_update.py
+++ src/test_update.py
@@ -1,17 +1,37 @@
 import os
 import requests
+import json
+import sys
 
 API_TOKEN = os.getenv("PROM_API_TOKEN")
-API_URL = "https://my.prom.ua/api/v1/products/list?limit=3"
+
+if len(sys.argv) != 3:
+    print("Вкажи: python src/test_update.py <external_id> <price>")
+    sys.exit(1)
+
+external_id = sys.argv[1]
+price = float(sys.argv[2])
+
+API_URL = "https://my.prom.ua/api/v1/products/set_prices"
 
 headers = {
     "Authorization": f"Bearer {API_TOKEN}",
     "Content-Type": "application/json"
 }
 
-print("➡️ Запит на products/list")
+payload = {
+    "prices": [
+        {
+            "external_id": external_id,
+            "price": price
+        }
+    ]
+}
 
-response = requests.get(API_URL, headers=headers)
+print("➡️ Відправляю як JSON:")
+print(json.dumps(payload, indent=2, ensure_ascii=False))
+
+response = requests.post(API_URL, headers=headers, json=payload)
 
 print("📥 Статус:", response.status_code)
 print("📥 Відповідь (text):", response.text)
